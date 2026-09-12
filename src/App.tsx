0ed02804e0cce0365f7d596a73822d936760b405
mayeendu <mayeendu@gmail.com>
2026-09-13 01:24:04 +0600

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h2 className="text-5xl text-red-500"> Hello World!</h2>
+      <button className="btn btn-secondary">Secondary</button>
+    </>
+  );
+}
+
+export default App;
